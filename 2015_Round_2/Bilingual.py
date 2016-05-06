c904203afcf1b27aa572f815ee1b37123dbc1a7b
Yizhuo Xie(Joy) <joylinkyizhuo@gmail.com>
2016-05-05 20:01:22 -0700

    tmp cpp code

--- 2015_Round_2/Bilingual.py
+++ 2015_Round_2/Bilingual.py
@@ -1,98 +1,109 @@
-inp = open('C-small-practice.in')
-out = open('B-large-practice.out', "w")
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<map>
+#include<queue>
+#include<sstream>
+#define MAXN 250
+#define MAXM 250
+#define ll long long
+using namespace std;
+int X[MAXN];
+const int INF = 1000000009;
+map<string, int> ID;
 
-debug_model = True
+int getID(string s) {
+    if (!ID.count(s))
+        ID[s] = ID.size();
+    return ID[s];
+}
+struct Dinic
+{
+    struct node
+    {
+        int x,y,c,next;
+    }line[MAXM];
+    int Lnum,_next[MAXN],dis[MAXN];
+    void initial(int n)
+    {
+        for (int i=0;i<=n;i++) _next[i]=-1;
+        Lnum=-1;
+    }
+    void addline(int x,int y,int c)
+    {
+        line[++Lnum].next=_next[x],_next[x]=Lnum;
+        line[Lnum].x=x,line[Lnum].y=y,line[Lnum].c=c;
+        line[++Lnum].next=_next[y],_next[y]=Lnum;
+        line[Lnum].x=y,line[Lnum].y=x,line[Lnum].c=c;
+    }
+    bool BFS(int s,int e)
+    {
+        queue<int> Q;
+        while (!Q.empty()) Q.pop();
+        memset(dis,0,sizeof(dis));
+        dis[s]=1;
+        Q.push(s);
+        while (!Q.empty())
+        {
+            int h,k;
+            h=Q.front(),Q.pop();
+            if (h==e) return dis[e];
+            for (k=_next[h];k!=0;k=line[k].next)
+                if (line[k].c && !dis[line[k].y])
+                    dis[line[k].y]=dis[h]+1,Q.push(line[k].y);
+        }
+        return false;
+    }
+    int dfs(int x,int flow,int e)
+    {
+        if (x==e) return flow;
+        int temp,cost=0;
+        for (int k=_next[x];k!=0;k=line[k].next)
+            if (line[k].c && dis[line[k].y]==dis[x]+1)
+            {
+                temp=dfs(line[k].y,min(flow-cost,line[k].c),e);
+                if (temp)
+                {
+                    line[k].c-=temp,line[k^1].c+=temp;
+                    cost+=temp;
+                    if (flow==cost) return cost;
+                }else dis[line[k].y]=-1;
+            }
+        return cost;
+    }
+    int MaxFlow(int s,int e)
+    {
+        int MaxFlow=0;
+        while (BFS(s,e))
+            MaxFlow+=dfs(s,INF,e);
+        return MaxFlow;
+    }
+}T;
 
-
-class edge(object):
-    def __init__(self, u, v, cap, flow):
-        self.u = u
-        self.v = v
-        self.cap = cap
-        self.flow = flow
-
-m = {}
-edges = []
-G = [[0] for j in xrange(250)]
-cur =  [0 for i in xrange(250)]
-d = [0 for i in xrange(250)]
-infi = 999999999999999
-
-s, t = -1, -1
-
-def add(u, v, cap) :
-    edges.append(edge(u, v, cap, 0))
-    G[u].append(len(edges) - 1)
-    edges.append(edge(v, u, 0, 0))
-    G[v].append(len(edges) - 1)
-
-def bfs():
-    vis = [0 for i in xrange(250)]
-    vis[s] = 1
-    queue = [s]
-    d[s] = 0
-    while queue:
-        u = queue.pop(0)
-        for i in xrange(1, len(G[u])):
-            e = edges[G[u][i]]
-            if vis[e.v] == 1 or e.cap == e.flow:
-                continue
-            d[e.v] = d[u]+1
-            vis[e.v]=1
-            queue.append(e.v)
-    return vis[t] > 0
-
-def dfs(u, a):
-    if u == t or a == 0:
-        return a
-    flow, f, i = 0, 0, cur[u]
-    for i in xrange(1, len(G[u])):
-        e = edges[G[u][i]]
-        f = dfs(e.v, min(a, e.cap-e.flow))
-        if d[e.v] == d[u]+1 and f > 0:
-            edges[G[u][i]].flow+=f
-            edges[G[u][i]^1].flow-=f
-            flow+=f
-            a-=f
-            if a == 0:
-                break
-
-    return flow
-
-def dinic():
-    flow, x = 0, 0
-    while bfs():
-        cur = [0 for i in xrange(250)]
-        x = dfs(s, infi)
-        while x != 0:
-            flow+=x
-            cur = [0 for i in xrange(250)]
-            x = dfs(s, infi)
-
-    return flow
-
-T = int(raw_input()) if debug_model else int(inp.readline())
-for i in xrange(T):
-    m = {}
-    s = 1
-    t = 2
-    N = int(raw_input()) if debug_model else int(inp.readline())
-    G = [[0] for j in xrange(250)]
-    d = [0 for x in xrange(250)]
-    for j in xrange(1, N+1):
-        string = raw_input().split()
-        for c in string:
-            if not c in m:
-                m[c] = len(m) + 1
-            add(j, N+2*m[c]-1, infi)
-            add(N+2*m[c], j, infi)
-    for j in xrange(len(m)):
-        add(N+2*j+1, N+2*j+2, 1)
-    if debug_model:
-        print "Case #" + str(i+1) + ": " + str(dinic())
-    else:
-        out.write("Case #" + str(i+1) + ": " + str(dinic()) + '\n')
-
-
-
-
+int main()
+{
+    int TT;
+    scanf("%d", &TT);
+    while (TT--) {
+        int n;
+        cin >> n;
+        ID.clear();
+        string s, t;
+        getline(cin, s);
+        for (int i = 1; i <= n; i++) {
+            getline(cin, s);
+            istringstream ssin(s);
+            while (ssin >> t) {
+                int x = getID(t);
+                T.addline(i, n + x * 2 - 1, INF);
+                T.addline(n + x * 2, i, INF);
+            }
+        }
+        for(int i=0; i<ID.size(); i++)
+            T.addline(n + i + i + 1, n + i * 2 + 2, 1);
+        cout<<T.MaxFlow(1, 2);
+    }
+    return 0;
+}
