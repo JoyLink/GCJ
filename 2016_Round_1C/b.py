694a60fd2e9a6d11d8d13624ce31663f7bf1f7da
Yizhuo Xie(Joy) <joylinkyizhuo@gmail.com>
2016-05-06 17:49:01 -0700

    2015-B-Drum Decorator

---
+++ 2016_Round_1C/b.py
@@ -0,0 +1,25 @@
+inp = open('C-large-practice.in')
+out = open('B-large-practice.out', "w")
+
+debug_model = True
+
+T = int(raw_input()) if debug_model else int(inp.readline())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if debug_model:
+    print
+else:
+    out.write()
